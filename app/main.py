014844c8807ca326a3659cab268b36dd12ef2b96
iracross1 <cross.i@hotmail.com>
2022-10-18 09:27:06 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ from .config import settings
 
 app = FastAPI()
 
-origins = ["https://www.google.com"]
+#origins = ["https://www.google.com"]
 
 app.add_middleware(
 	CORSMiddleware,
-	allow_orgins=origins,
+	allow_orgins=[*],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
